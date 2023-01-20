0e6c5efaf43b02129257eb0175de3243b15816ee
GMM-1 <and21499@uvg.edu.gt>
2023-01-20 09:57:58 -0600

    progreso postlab
    configuracion del tmr1 con su interrupcion listo

--- lab01.c
+++ lab01.c
@@ -12,25 +12,25 @@ creado: 18-01-2023
  */
 
 // CONFIG1
-#pragma config FOSC = INTRC_NOCLKOUT// Oscillator Selection bits (INTOSCIO oscillator: I/O function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
-#pragma config WDTE = OFF        // Watchdog Timer Enable bit (WDT enabled)
-#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
-#pragma config MCLRE = OFF       // RE3/MCLR pin function select bit (RE3/MCLR pin function is MCLR)
-#pragma config CP = OFF         // Code Protection bit (Program memory code protection is disabled)
-#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
-#pragma config BOREN = OFF       // Brown Out Reset Selection bits (BOR enabled)
-#pragma config IESO = OFF        // Internal External Switchover bit (Internal/External Switchover mode is enabled)
-#pragma config FCMEN = OFF       // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is enabled)
-#pragma config LVP = OFF         // Low Voltage Programming Enable bit (RB3/PGM pin has PGM function, low voltage programming enabled)
+#pragma config FOSC = INTRC_NOCLKOUT  // Oscillator Selection bits (INTOSCIO oscillator: I/O function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
+#pragma config WDTE = OFF             // Watchdog Timer Enable bit (WDT enabled)
+#pragma config PWRTE = OFF            // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = OFF            // RE3/MCLR pin function select bit (RE3/MCLR pin function is MCLR)
+#pragma config CP = OFF               // Code Protection bit (Program memory code protection is disabled)
+      #pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
+#pragma config BOREN = OFF            // Brown Out Reset Selection bits (BOR enabled)
+#pragma config IESO = OFF             // Internal External Switchover bit (Internal/External Switchover mode is enabled)
+#pragma config FCMEN = OFF            // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is enabled)
+#pragma config LVP = OFF              // Low Voltage Programming Enable bit (RB3/PGM pin has PGM function, low voltage programming enabled)
 
 // CONFIG2
-#pragma config BOR4V = BOR40V   // Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
-#pragma config WRT = OFF        // Flash Program Memory Self Write Enable bits (Write protection off)
+#pragma config BOR4V = BOR40V         // Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
+#pragma config WRT = OFF              // Flash Program Memory Self Write Enable bits (Write protection off)
 
 #define _XTAL_FREQ 4000000
 #include <xc.h>
 #include <stdio.h>
-#include "ADC.h"      //libreria para el manejo del ADC
+#include "ADC.h"      //libreria para la configuracion del ADC
 
 ////////////////////////////////////////////////////////////////////////////////
 // VARIABLES
@@ -47,8 +47,11 @@ int valor_adc;
 void setupINTOSC(void);
 void setupPORTS(void);
 void setupINTERRUPT(void);
+void setupTMR1(void);
 void contador(void);
 void conversion_adc(void);
+void reseteo_TMR1(void);
+
 
 ////////////////////////////////////////////////////////////////////////////////
 // Rutina de INTERRUPCIONES
@@ -56,17 +59,25 @@ void conversion_adc(void);
 
 void __interrupt() isr(void)
 {
+
+  //INTERRUPCION DEL TMR1H
+  if(PIR1bits.TMR1IF) //se enciende la bandera cada segundo
+  {
+    PORTC++;
+    reseteo_TMR1();
+  }
+
   //INTERRUPCION DEL PORTB
     if (INTCONbits.RBIF == 1)//revisar bandera de interrupcion
     {
         contador();
         INTCONbits.RBIF = 0; //limpiar bandera
     }
+
   //INTERRUPCION DEL ADC
     if (PIR1bits.ADIF) //revisar bandera de interrupcion
     {
       conversion_adc();
-
     }
 }
 
@@ -79,6 +90,7 @@ void main(void)
     setupINTOSC();    //configuracion del reloj oscilador
     setupPORTS();     //configuracion de los puertos
     setupINTERRUPT(); //configuracion de interrupt on change
+    setupTMR1();      //configuracion para el timer
     Init_ADC(AN12);   //Inicializacion del ADC para canal AN12
 
 
@@ -115,6 +127,8 @@ void setupPORTS(void)
   PORTD = 0;
   TRISA = 0;
   PORTA = 0;
+  TRISC = 0;
+  PORTC = 0;
 
   //entradas
   TRISBbits.TRISB7 = 1;
@@ -128,15 +142,34 @@ void setupINTERRUPT(void)
   //interrupcion del puerto B (push buttons)
   INTCONbits.RBIE = 1; //interrupciones del puerto B
   OPTION_REGbits.nRBPU = 0; // habilitar pull ups individuales en B
-  WPUBbits.WPUB7 = 1;//pullup en B7
-  WPUBbits.WPUB6 = 1;//pullup en B6
-  IOCBbits.IOCB7 = 1;//interrupt on change en B7
-  IOCBbits.IOCB6 = 1;//interruot on change en B6
+  WPUBbits.WPUB7 = 1;       //pullup en B7
+  WPUBbits.WPUB6 = 1;       //pullup en B6
+  IOCBbits.IOCB7 = 1;       //interrupt on change en B7
+  IOCBbits.IOCB6 = 1;       //interruot on change en B6
+  INTCONbits.RBIF = 0;      //bandera apagada
 
   INTCONbits.GIE = 1; //interrupciones globales activadas
   INTCONbits.PEIE = 1; //interrupciones de perifericos
 
   PIR1bits.ADIF = 0;      // A/D conversion no ha empezado o completado
+
+  //interrupcion del timer 1
+  PIE1bits.TMR1IE = 1;    // interrupcion del TMR1H
+  PIR1bits.TMR1IF = 0;    //bandera apagada
+
+}
+
+void setupTMR1(void)
+{
+// Timer1 Registers Prescaler= 1 - TMR1 Preset = 65036 - Freq = 500.00 Hz - Period = 0.002000 seconds
+  T1CONbits.T1CKPS1 = 0;   // bits 5-4  Prescaler Rate Select bits
+  T1CONbits.T1CKPS0 = 0;   // bit 4
+  T1CONbits.T1OSCEN = 0;   // bit 3 Timer1 Oscillator Enable Control bit 1 = on
+  T1CONbits.T1SYNC = 1;    // bit 2 Timer1 External Clock Input Synchronization Control bit...1 = Do not synchronize external clock input
+  T1CONbits.TMR1CS = 0;    // bit 1 Timer1 Clock Source Select bit...0 = Internal clock (FOSC/4)
+  T1CONbits.TMR1ON = 1;    // bit 0 enables timer
+  // Reset del tmr1
+  reseteo_TMR1();
 }
 
 void contador(void)
@@ -170,4 +203,11 @@ void conversion_adc(void)
 
   //delay de funcionamiento
   __delay_ms(20);
+}
+
+void reseteo_TMR1(void)
+{
+  TMR1H = 254;
+  TMR1L = 12;
+  PIR1bits.TMR1IF = 0; //apagamos la bandera
 }
